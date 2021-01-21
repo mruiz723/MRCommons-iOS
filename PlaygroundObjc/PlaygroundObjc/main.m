cd2c7484bf52e6c972b937b0e1de55bc32f958ec
Marlon Ruiz <mruiz723@gmail.com>
2021-01-21 16:37:45 -0500

    Fix some issues.

---
+++ PlaygroundObjc/PlaygroundObjc/main.m
@@ -0,0 +1,31 @@
+//
+//  main.m
+//  PlaygroundObjc
+//
+//  Created by Marlon David Ruiz Arroyave on 18/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+        
+    }
+    return 0;
+}
+
+@interface Test : NSObject {
+//    +(NSInteger) isPresent: (NSString *) root : (NSInteger)key;
+}
+
+@end
+@implementation
+    +(NSInteger) isPresent: (NSString *) root : (NSInteger)key {
+        
+    }
+
+@end
+
