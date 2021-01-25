a0a512d4d72f118d3f8e50e8f2e5e34938a3a155
Marlon Ruiz <mruiz723@gmail.com>
2021-01-25 15:21:30 -0500

    Create Package.

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MRCommons-iOS",
+    platforms: [
+      .iOS(.v13)
+    ],
+    products: [
+      .library(
+        name: "MRCommons",
+        targets: ["MRCommons"]),
+    ],
+    targets: [
+      .binaryTarget(
+        name: "MRCommons",
+        url: "https://github.com/mruiz723/MRCommons-iOS/releases/download/1.0.0/MRCommons.xcframework.zip",
+        checksum: "724635f4b74de3dd3fd5574daca7f83c036a2a9e34169ae36720e9825aa7c31a"
+      )
+    ]
+)
